fec9e10d16431669318cc39c10c8d1442a8fc429
Ajay Kaarthic Jeysree <ajay.kaarthic@gmail.com>
2022-10-08 11:32:22 -0500

    Add the leetcode problem link

--- neetcode.io/2_valid_anagram.py
+++ neetcode.io/2_valid_anagram.py
@@ -28,10 +28,18 @@ class Solution:
         
         # declare an empty dictionary to keep track of alphabet count
         count_s = {}
-        count_t = {}
         
         # Insert an element to the dict if not available otherwise increase the count of element
         for i in range(len(s)):
             count_s[s[i]] = 1 + count_s.get(s[i], 0)
-            count_t[t[i]] = 1 + count_t.get(t[i], 0)
-        return count_s == count_t
+        
+        # Check if char in t is available in s
+        for i in t:
+            if i in count_s:
+                count_s[i] = count_s[i] - 1
+        
+        # If all the values in the dictionary count_s is 0, it means t is an anagram of s
+        for val in count_s.values():
+            if val != 0:
+                return False
+        return True
